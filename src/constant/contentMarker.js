2858a243cc6866790e4b00eb855a896f9cf40bf3
이국신 <nw4611@naver.com>
2023-05-25 12:07:13 +0900

    feat: MarkerImage 적용
    close #44

---
+++ src/constant/contentMarker.js
@@ -0,0 +1,11 @@
+const contentsMarker = {
+    12: 'attraction.png',
+    14: 'culture.png',
+    15: 'festival.png',
+    25: "course.png",
+    28: "sports.png",
+    32: "hotel.png",
+    38: 'shopping.png',
+    39: "food.png",
+};
+export default contentsMarker;
